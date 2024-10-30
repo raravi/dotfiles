5c189bf65add953f3fee453d86bc721b4d7bfa7a
Amith Raravi <amith.raravi@gmail.com>
2024-10-31 00:05:17 +0100

    add config, leader key and keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,13 +45,36 @@ vim.opt.syntax         = "on"
 -- Enabling true color terminal allows Neovim to utilize 24-bit RGB color values, providing 
 -- a more extensive and accurate range of colors
 vim.opt.termguicolors  = true
-require("bufferline").setup{}
+require("bufferline").setup{
+  options = {
+    indicator = {
+      icon = '/ ',
+      style = 'icon'
+    },
+    separator_style = "slant",
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "Neotree",
+        separator = true
+      }
+    }
+  }
+}
 
 -- Enable Spellchecking
 vim.opt.spell          = true
 vim.opt.spelllang      = "en_us"
 
+vim.g.mapleader = ";"
+
 -- ESC in insert & visual mode
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("v", "jk", "<ESC>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
